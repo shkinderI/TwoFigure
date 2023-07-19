6c8d947732f0c44be708d4451689ffd919d14c72
shkinderI <shkinder.ilia@gmail.com>
2023-07-19 20:42:17 +0300

    Initial Commit

---
+++ TwoFigure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoFigure
+//
+//  Created by Ilia on 19.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
